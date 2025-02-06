4563f701381ca97a9299f31dd1d54b87cd8e28cb
telminov <telminov@soft-way.biz>
2025-02-06 10:09:42 +0300

    обновил версию пакета и заметки по загрузки его в pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 # coding: utf-8
-# python setup.py sdist register bdist_egg upload
+# python -m pip install certifi
+# export SSL_CERT_FILE=$(python -m certifi)
+# python setup.py sdist
+# twine upload dist/*
 from setuptools import setup, find_packages
 
 setup(
     name='sw-django-logger',
-    version='0.0.17',
+    version='0.0.18',
     description='Log handling and representing for django projects',
     author='Telminov Sergey',
     author_email='sergey@telminov.ru',
